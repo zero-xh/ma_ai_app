3505862ca634cdadb1393304d775cf989d64c8d6
zero-xh <93333954+zero-xh@users.noreply.github.com>
2026-03-05 17:41:08 +0800

    feat:连接前后端

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,14 +2,14 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [message, setMessage] = useState('hello')
+  const [message, setMessage] = useState('')
   useEffect(() => {
     fetch('/api/hello').then(res => res.json())
       .then(data => setMessage(data.message))
   }, [])
 
   return (
-    <p className='font-bold p-4 text-3xl'>你好</p>
+    <p className='font-bold p-4 text-3xl'>{message}</p>
   )
 }
 
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -10,6 +10,12 @@ app.get('/', (req: Request, res: Response) => {
     res.send('hello world')
 })
 
+app.get('/api/hello', (req: Request, res: Response) => {
+    res.json({
+        message: 'hello world'
+    })
+})
+
 app.listen(port, () => {
     console.log(`服务器运行在 http://localhost:${port} 运行中`)
 })
