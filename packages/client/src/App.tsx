7aae3729922fa25e5819d38c33ef09cecea00bda
zero-xh <93333954+zero-xh@users.noreply.github.com>
2026-03-05 17:55:44 +0800

    feat:设置shaden UI

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   const [message, setMessage] = useState('')
@@ -9,7 +10,10 @@ function App() {
   }, [])
 
   return (
-    <p className='font-bold p-4 text-3xl'>{message}</p>
+    <div className='p-4'>
+      <p className='font-bold p-4 text-3xl'>{message}</p>
+      <Button>点击</Button>
+    </div>
   )
 }
 
