72c8314cce155a51dea5e13e6a41e9795455be75
zero-xh <93333954+zero-xh@users.noreply.github.com>
2026-03-05 19:14:24 +0800

    feat:设置prettier

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,20 +1,21 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import { Button } from './components/ui/button'
+import { useEffect, useState } from 'react';
+import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState('')
-  useEffect(() => {
-    fetch('/api/hello').then(res => res.json())
-      .then(data => setMessage(data.message))
-  }, [])
+   const [message, setMessage] = useState('');
+   useEffect(() => {
+      fetch('/api/hello')
+         .then((res) => res.json())
+         .then((data) => setMessage(data.message));
+   }, []);
 
-  return (
-    <div className='p-4'>
-      <p className='font-bold p-4 text-3xl'>{message}</p>
-      <Button>点击</Button>
-    </div>
-  )
+   return (
+      <div className="p-4">
+         <p className="font-bold p-4 text-3xl">{message}</p>
+         <Button>点击</Button>
+      </div>
+   );
 }
 
-export default App
+export default App;
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,21 +1,21 @@
-import express from "express";
-import type { Request, Response } from "express";
-import dotenv from 'dotenv'
+import express from 'express';
+import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('hello world')
-})
+   res.send('hello world');
+});
 
 app.get('/api/hello', (req: Request, res: Response) => {
-    res.json({
-        message: 'hello world'
-    })
-})
+   res.json({
+      message: 'hello world',
+   });
+});
 
 app.listen(port, () => {
-    console.log(`服务器运行在 http://localhost:${port} 运行中`)
-})
+   console.log(`服务器运行在 http://localhost:${port} 运行中`);
+});
